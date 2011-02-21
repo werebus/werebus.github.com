39e9f2d4a049d3bc6f2d463555c45b59fa8de883
Matt Moretti <werebus@gmail.com>
2011-02-21 03:42:57 -0500

    Temporary change to http root

--- config.rb
+++ config.rb
@@ -2,10 +2,11 @@ require 'html5-boilerplate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "file:///Users/matt/projects/personal/"
 css_dir = "css"
 sass_dir = "src"
 images_dir = "images"
 javascripts_dir = "js"
+output_style = :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
