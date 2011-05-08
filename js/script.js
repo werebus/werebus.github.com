babf88c43ed3e32578c4510437927d6efe66a8b8
Matt Moretti <werebus@gmail.com>
2011-05-07 23:37:22 -0400

    jQuery hover badges

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-/* Author: 
+/* Author: Matt Moretti
 
 */
 
-
+$('.badge').hover(
+  function(){$(this).animate({'padding-bottom': '20px'}, {queue: false})}, 
+  function(){$(this).animate({'padding-bottom': '0px'}, {queue: false})}
+)
 
 
 
