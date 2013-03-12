bc203d8e719e12ba84e54c789671465892489c26
Matt Moretti <werebus@gmail.com>
2013-03-12 18:04:43 -0400

    Cheap(lame) cache-circumvention

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ $('.badge').hover(
   function(){$(this).animate({'padding-bottom': '0px'}, {queue: false})}
 )
 
+var d = new Date();
+var ds = [d.getFullYear(), d.getMonth() + 1].join('');
+$('#gravatar').attr('src', 'http://www.gravatar.com/avatar/9a82bcc06a07030f05996fdb76a2f2e5?s=120&v=' + ds);
 
 
 
